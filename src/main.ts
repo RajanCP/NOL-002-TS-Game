39715eff0b6b498835bd54a0a54f4a9520846a0c
RajanCP <rjnptl2000@gmail.com>
2025-07-08 15:54:15 +0100

    TS (Typescript) started, User input can be accessed, Display paragraph is shown

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
 import "./style.scss";
+
+
+const displayParagraph = document.querySelector("#paragraph-display") as HTMLDivElement;
+const inputParagraph = document.querySelector("#paragraph-input") as HTMLInputElement;
+
+const sentences = ["This is dummy text to copy"];
+
+displayParagraph.innerText = sentences[0];
+
